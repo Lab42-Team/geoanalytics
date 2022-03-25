3d37a421088f68b167d8812abe8be59aee3c44ce
LedZeppe1in <tualatin32@mail.ru>
2022-03-25 11:06:31 +0800

    Added determine_hazard_classes_for_forest_districts function

--- geoanalytics/geo_utility.py
+++ geoanalytics/geo_utility.py
@@ -184,7 +184,7 @@ def determine_intersection_with_forestry(fires_dict, forestry_dict):
     return fires_dict
 
 
-def determine_nearest_weather_station(fires_dict, weather_stations_dict):
+def determine_nearest_weather_station_to_fire(fires_dict, weather_stations_dict):
     """
     Определение ближайших к пожарам метеостанций.
     :param fires_dict: словарь с данными по пожарам
@@ -813,6 +813,78 @@ def delete_winter_fires(fires_dict):
     print("Full time: " + str(datetime.now() - start_full_time))
 
     return fires_dict
+
+
+def determine_hazard_classes_for_forest_districts(forest_districts_dict, forest_hazard_classes_dict):
+    """
+    Определение классов опасности лесов для лесных кварталов.
+    :param forest_districts_dict: словарь с данными по лесным кварталам
+    :param forest_hazard_classes_dict: словарь с данными по классам опасностей лесов
+    :return: дополненный словарь с данными по лесным кварталам
+    """
+    start_full_time = datetime.now()
+    forest_district_index = 0
+    defined_hazard_class_number = 0
+    # Обход лесных кварталов
+    for forest_district_item in forest_districts_dict.values():
+        start_time = datetime.now()
+        forest_hazard_classes = []
+        forest_district_index += 1
+        fd_municipality = forest_district_item["name_in"]
+        fd_forest_plot = forest_district_item["uch_l_ru"]
+        fd_dacha = forest_district_item["dacha_ru"]
+        fd_kv = forest_district_item["kv"]
+        if str(fd_municipality) != "nan":
+            fd_municipality = re.sub(r"[-']", "", fd_municipality)
+            fd_municipality = fd_municipality.lower()
+        if str(fd_forest_plot) != "nan":
+            fd_forest_plot = re.sub(r"[-']", "", fd_forest_plot)
+            fd_forest_plot = fd_forest_plot.lower()
+        if str(fd_dacha) != "nan":
+            fd_dacha = re.sub(r"[-']", "", fd_dacha)
+            fd_dacha = fd_dacha.lower()
+        if str(fd_municipality) != "nan" and str(fd_forest_plot) != "nan" and str(fd_dacha) != "nan":
+            # Обход данных по классам опасностей лесов
+            for forest_hazard_classes_item in forest_hazard_classes_dict.values():
+                fhc_municipality = forest_hazard_classes_item["municipality"]
+                fhc_municipality = re.sub(r"[-']", "", fhc_municipality)
+                fhc_municipality = fhc_municipality.lower()
+                fhc_forest_plot = forest_hazard_classes_item["forest_plot"]
+                fhc_forest_plot = re.sub(r"[-']", "", fhc_forest_plot)
+                fhc_forest_plot = fhc_forest_plot.lower()
+                fhc_dacha = forest_hazard_classes_item["dacha"]
+                fhc_dacha = re.sub(r"[-']", "", fhc_dacha)
+                fhc_dacha = fhc_dacha.lower()
+                # Вычисление расстояния Левенштейна
+                levenshtein_distance_for_municipality = distance(fhc_municipality, fd_municipality)
+                # Вычисление расстояния Левенштейна
+                levenshtein_distance_for_forest_plot = distance(fhc_forest_plot, fd_forest_plot)
+                # Вычисление расстояния Левенштейна
+                levenshtein_distance_for_dacha = distance(fhc_dacha, fd_dacha)
+                total_levenshtein_distance = levenshtein_distance_for_municipality + \
+                                             levenshtein_distance_for_forest_plot + levenshtein_distance_for_dacha
+                if total_levenshtein_distance < 4:
+                    for forest_district_number in forest_hazard_classes_item["forest_districts"]:
+                        if str(fd_kv) and str(forest_district_number):
+                            if int(forest_district_number) == int(fd_kv):
+                                # Формирование списка определенных классов опасности лесов
+                                forest_hazard_classes.append(str(forest_hazard_classes_item["hazard_class"]))
+
+        # Формирование данных по классам опасности лесов
+        if forest_hazard_classes:
+            forest_district_item["hazard_classes"] = str(forest_hazard_classes)
+            defined_hazard_class_number += 1
+            print("Классы опасности: " + forest_district_item["hazard_classes"])
+
+        print("Строка " + str(forest_district_index) + ": " + str(datetime.now() - start_time))
+
+    # Вычисление точности определения класса опасности
+    accuracy = defined_hazard_class_number / forest_district_index
+    print("Accuracy: " + str(accuracy))
+
+    print("Full time: " + str(datetime.now() - start_full_time))
+
+    return forest_districts_dict
 
 
 def identify_fire_by_dates(fires_dict):
